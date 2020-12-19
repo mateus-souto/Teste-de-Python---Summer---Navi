63e7b4e1ff551eb258f6aec9d4c4bb93b8b9ce98
mateus-souto <76265953+mateus-souto@users.noreply.github.com>
2020-12-19 18:02:44 -0300

    Create exercicio3testenavi.py

---
+++ exercicio3testenavi.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+
+vetor = []
+
+for i in range (10):
+
+    if (i % 2 == 0 ):
+        valor = 3i+7*(math.factorial(i))
+    else:
+        valor = 2i+4*(math.log(i))
+
+    vetor.append(valor)
+
+
+print("A posição do maior elemento é",np.argmax(vetor))
+
+print("A média dos elementos é",np.mean(vetor))
